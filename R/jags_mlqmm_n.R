47e84446fcec5f39fa4b55b045759ae0aab8f62e
abarbieri <antoine.barbieri@gmail.com>
2020-07-01 09:48:45 +0200

    correction of mistakes

--- R/jags_mlqmm_n.R
+++ R/jags_mlqmm_n.R
@@ -1,4 +1,4 @@
-jags_3mlqmm_n <-
+jags_mlqmm_n <-
   function (...) {
     for(q in 1:Q){
       c1[q] <- (1-2*tau[q])/(tau[q]*(1-tau[q]))
@@ -14,7 +14,7 @@ jags_3mlqmm_n <-
           prec[j, qq] <- 1/(W[j, qq]*sigma[qq]*c2[qq])
           # first quantile distribution
           y[j, 1] ~ dnorm(mu[j, qq], prec[j, qq])
-          mu3[j] <- inprod(beta[3, 1:ncX], X[j, 1:ncX]) + inprod(b[i, (ncU*(qq-1)+1):(qq*ncU)], U[j, 1:ncU]) + c1[qq]*W[j, qq]
+          mu[j] <- inprod(beta[3, 1:ncX], X[j, 1:ncX]) + inprod(b[i, (ncU*(qq-1)+1):(qq*ncU)], U[j, 1:ncU]) + c1[qq]*W[j, qq]
         }#end of qq loop
       }#end of j loop
       # random effects
